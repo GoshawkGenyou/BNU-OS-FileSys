4f57e1f4e0be1ec7fa6633c71709ba348e50bef9
mayoix <125663435+mayoix@users.noreply.github.com>
2024-12-09 18:48:44 +0800

    delete.cpp
    没有释放删除文件占用的磁盘块，导致块在文件系统中仍然被标记为已使用。因此需要正确释放文件占用的所有磁盘块。

--- source/delete.cpp
+++ source/delete.cpp
@@ -7,15 +7,26 @@ void delete(char *filename){
 
 	dirid = namei(filename);
 	if (dirid == -1){
-		printf("�ļ������ڣ�����!\n"); 
+		printf("文件不存在，请检查!\n"); 
 		return;
 	}
 	inode = iget(dir.direct[dirid].d_ino);
 	if(!(inode->di_mode &DIFILE)){
-		printf("�������ļ������飡\n");
+		printf("对象不是文件，请检查！\n");
 		iput(inode);
 		return;
 	}
+
+	int blocks = (inode->di_size + BLOCKSIZ - 1) / BLOCKSIZ; // 计算文件占用的总磁盘块数
+	for (int i = 0; i < blocks; i++) {
+		if (inode->di_addr[i] != 0) {
+			bfree(inode->di_addr[i]); // 释放磁盘块
+			inode->di_addr[i] = 0; // 将磁盘块号设置为0
+		}
+	}
+
+	inode->di_size = 0; // 重置文件大小为0
+	
 	dir.direct[dirid].d_ino = DIEMPTY;
 	dir.size --;
 	inode->di_number--;
